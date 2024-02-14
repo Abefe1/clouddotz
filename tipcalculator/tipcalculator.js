1aeab4ddccabb23153f82f84e3ae993ebfc38dee
Isaiah Okon <izaya4real@gmail.com>
2024-02-14 21:36:26 +0100

    Update on naming convention

--- tipcalculator/tipcalculator.js
+++ tipcalculator/tipcalculator.js
@@ -1,27 +1,25 @@
 // this is used to add "onload" event
 window.onload = () => {
   //this is used to call the function for calculating the tip.
-
-  document.querySelector("#solve").onclick = calTip;
+  document.querySelector("#solve").onclick = calculateTip;
 };
 
-function calTip() {
+function calculateTip() {
   // first get the value of input values from user with the querySelector method or getByElementId
   let amount = document.querySelector("#amount").value;
-  let percent = document.querySelector("#percentage").value;
+  let percentage = document.querySelector("#percentage").value;
   let people = document.querySelector("#people").value;
-  let currency = document.querySelector("#select").value;
+  let currency = document.querySelector("#selectCurrency").value;
 
-  // this is just to display our collected values to console to be sure that the user input was collected
+  // this is just to display our collected values to console to be sure that the user input were collected
   console.log(amount);
-  console.log(percent);
+  console.log(percentage);
   console.log(people);
 
-  /* we don't want user to calculate with incomplete fields; thus,
-    we set a condition to ascertain none of the fields is null.*/
+  /* we don't want user to calculate with incomplete fields; thus, we set a condition to ascertain none of the fields is null.*/
   if (
     amount === "" ||
-    percent === "" ||
+    percentage === "" ||
     people === "" ||
     currency === "Currency"
   ) {
@@ -34,31 +32,34 @@ function calTip() {
   /* the following lines is to calculate the desired output.
     and also parsing the output to float for flexibilty in calculation*/
 
-  let amountf =
+  let parseAmount =
     parseFloat(amount); /* parsing the amount to float for flexibilty*/
-  let tip = parseFloat(
-    (percent / 100) * amount
+
+  let tipAmt = parseFloat(
+    (percentage / 100) * amount
   ); /* calculating the percentage based on the input*/
-  let totalAmountToPay = amountf + parseFloat(tip);
+
+  let totalAmountToPay = parseAmount + parseFloat(tipAmt);
+
   let tipPerPerson = parseFloat(
-    tip / people
+    tipAmt / people
   ); /* line to determine share of each worker involved */
 
-  /*Assign output to the appropriate out-box paragraph, fixing all to 2decimal places*/
-  document.querySelector("#outAmount").innerHTML =
-    currency + amountf.toFixed(2);
-  document.querySelector("#tamount").innerHTML = currency + tip.toFixed(2);
-  document.querySelector("#total").innerHTML =
+  /*Assign output to the appropriate outputTag paragraph, fixing all to 2decimal places*/
+  document.querySelector("#outputAmount").innerHTML =
+    currency + parseAmount.toFixed(2);
+
+  document.querySelector("#tipAmount").innerHTML = currency + tipAmt.toFixed(2);
+
+  document.querySelector("#totalAmt").innerHTML =
     currency + totalAmountToPay.toFixed(2);
-  document.querySelector("#per").innerHTML = currency + tipPerPerson.toFixed(2);
+
+  document.querySelector("#perPerson").innerHTML =
+    currency + tipPerPerson.toFixed(2);
 }
 
 //function to refresh the page/form
 function refresh() {
-  let rset = document.querySelector("#formData");
-  rset.reset();
+  let resetBtn = document.querySelector("#formData");
+  resetBtn.reset();
 }
-
-// window.addEventListener("click", ()=> {
-//     document.querySelector('#solve') =calTip;
-// })
